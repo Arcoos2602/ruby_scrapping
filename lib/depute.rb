5bea7974e9c15d2ff85c4354bdb99c8a33bf14a8
Arcoos2602 <cordonnier.thomas2@gmail.com>
2020-07-17 07:50:56 +0200

    depute emails done correctly + tests

--- lib/depute.rb
+++ lib/depute.rb
@@ -3,7 +3,10 @@ require 'nokogiri'
 require 'open-uri'
 
 def get_email(i)
-  get_first_name(i).downcase + "." + get_last_name(i).downcase + "@" + "assemblee-nationale.fr"
+  lien = i.css('td[1]/a/@href').text
+  page = Nokogiri::HTML(URI.open("http://www2.assemblee-nationale.fr#{lien}"))
+  tmp = page.css('div.contenu-principal.en-direct-commission.clearfix > div > dl > dd:nth-child(8) > ul > li:nth-child(2) > a')
+  email = tmp.text
 end
 
 def get_first_name(i)
@@ -19,7 +22,8 @@ end
 def depute
   page = Nokogiri::HTML(URI.open('http://www2.assemblee-nationale.fr/deputes/liste/tableau'))
   tab = page.css('//table/tbody/tr')
-  tab[0..5].map { |i| { "first name" => get_first_name(i), "last_name" => get_last_name(i), "email" => get_email(i)} }
+  tab.map { |i| { "first_name" => get_first_name(i), "last_name" => get_last_name(i), "email" => get_email(i)} }
+  #tab.map[0..5] { |i| { "first_name" => get_first_name(i), "last_name" => get_last_name(i), "email" => get_email(i)} } to be quicker
 end
 
 pp depute
--- spec/depute_spec.rb
+++ spec/depute_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/mairie.rb'
+require_relative '../lib/depute'
 
 describe "depute" do 
   it "It should return an array of hash" do
@@ -6,9 +6,9 @@ describe "depute" do
     expect((depute)).to be_a_kind_of Array
   end
   it "It should return at least the first town of all the list with the email" do
-    expect(depute[0]).to eq("first_name" => "Damien", "last_name" => "mairie.ableiges95@wanadoo.fr", "email" => "damien.abad@assemblee-nationale.fr")
+    expect(depute[0]).to eq("first_name" => "Damien", "last_name" => "Abad", "email" => "damien.abad@assemblee-nationale.fr")
   end
-  it "It should return 185 town" do
+  it "It should return 577 deputes" do
     expect(depute.count).to eq(577)
   end
 end
