6fd51f9562bd6537063d46cb5e05a290fad1ce8b
Arcoos2602 <cordonnier.thomas2@gmail.com>
2020-07-17 08:37:27 +0200

    useless comments deleted

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -7,11 +7,3 @@ def crypto_scrapper
   tmp = page.css('/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr')
   crypto_name_array = (tmp.map { |tr| { tr.css('td[2]').text => tr.css('td[5]').text } })
 end
-
-#begin
-  #crypto_scrapper
-#rescue StandardError =>s e
-  #puts 'ERROR: crypto_scrapper!'
-#end
-
-#crypto_scrapper
